361de8ff36fe027ceb4beca2147385a56d5850d1
Koman atsé <komanatsefabriceigor@gmail.com>
2022-08-19 17:49:46 +0400

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import * as signalR from "@microsoft/signalr";
 
+const baseUrl = "https://functionapp120220819172503.azurewebsites.net";
+// const App : React.FC = ()=>{
+//   const hubCon = new signalR
+// }
 function App() {
+  const hubCon = new signalR.HubConnectionBuilder()
+    .withUrl(`${baseUrl}/api`)
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+  useEffect(() => {
+    hubCon.start();
+
+    // fetch("http://localhost:7071/api/HttpExample1")
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //   });
+  }, []);
+  var list = [];
+  const Message = ({ HubConnection }) => {
+    const [date, setDate] = useState();
+    useEffect(() => {
+      HubConnection.on("newMessage", (message) => {
+        list.push(message);
+        setDate(new Date());
+      });
+    }, []);
+
+    return list.map((msg, index) => (
+      <p key={`message${index}`}>
+        <strong>{msg.sender}</strong> {msg.text}
+      </p>
+    ));
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +53,7 @@ function App() {
         >
           Learn React
         </a>
+        <Message HubConnection={hubCon} />
       </header>
     </div>
   );
